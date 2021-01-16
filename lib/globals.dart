1e85ce77716e2996977fec2fbd2990138e71effe
Abdulmalik Abubakar <abdul.abubakar@centrifugegroup.com>
2021-01-16 19:20:42 +0100

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 Color kPrimaryColorLight = Color.fromRGBO(0, 0, 20, 1);
 Color kPrimaryColorDark = Color.fromRGBO(0, 0, 13, 1);
 
-String endpointBaseUrl = 'http://192.168.43.91:8080/api/v1';
+String endpointBaseUrl = 'https://ronen-games.herokuapp.com//api/v1';
+// String endpointBaseUrl = 'http://192.168.43.91:8080/api/v1';
